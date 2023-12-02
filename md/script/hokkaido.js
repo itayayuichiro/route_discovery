0329c2a6290721e369f26603245c05a4cd91f179
itayayuichiro <mr.itaya.magic@gmail.com>
2023-12-02 09:39:22 +0900

    push

--- md/script/hokkaido.js
+++ md/script/hokkaido.js
@@ -1,8 +1,8 @@
 var result = [...document.querySelectorAll('.spotList dl')].map(dl => {
     var href = dl.querySelector('a').href
     var title = dl.querySelector('dt').innerText
-    var img = a.querySelector('img').src
-    return href + "," + title + "," + img
+    var img = dl.querySelector('img').src
+    return '"hokkaido","', href + '","' + title + '","' + img + '"'
 }).join("\n")
 
 console.log(result)
--- md/script/tabiiro.js
+++ md/script/tabiiro.js
@@ -1,8 +1,19 @@
 var result = [...document.getElementsByClassName('ren-article-card')].map(a => {
     var href = a.href
-    var title = a.getElementsByClassName('ren-article-card__ttl')[0].innerText
+    var title = a.getElementsByClassName('ren-article-card__ttl')[0].innerText.replace(/"/g, "'")
     var img = a.querySelector('.ren-article-card__img img').src
-    return href + "," + title + "," + img
+
+    const result = `<a href="${href}" class="list_content">` + "\n" +
+        `<div class="list_img">` + "\n" +
+            `<img src="${img}" />` + "\n" +
+        `</div>` + "\n" +
+        `<div class="list_text">` + "\n" +
+            `${title}` + "\n" +
+            `<div class="from">from HOKKAIDO LOVE!</div>` + "\n" +
+        `</div>` + "\n" +
+    `</a>` + "\n"
+
+    return result
 }).join("\n")
 
 console.log(result)
